18afaee77eb1780cfde8bc94f8994daa2cc3f854
HubertWebDeveloper <145841547+HubertWebDeveloper@users.noreply.github.com>
2024-01-11 14:05:05 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form">
+          <img src="../MyOwnerLogo.png" alt="">
+          <form action="#">
+            <textarea name="" cols="33" rows="5" placeholder="Write the comment here!!"></textarea>
+            <div class="buttons">
+                <button type="submit" name="reset" class="reset">Reset</button>
+                <button type="submit" name="comment" class="comment">Comment</button>
+            </div>
+          </form>
+        </div>
+        <div class="msg">
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!.</p>
+            </div>
+        </div>
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!, your message will display here after comment!.</p>
+            </div>
+        </div>
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!.</p>
+            </div>
+        </div>
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!.</p>
+            </div>
+        </div>
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!.</p>
+            </div>
+        </div>
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!, your message will display here after comment!.</p>
+            </div>
+        </div>
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!.</p>
+            </div>
+        </div>
+        <div class="results">
+            <img src="../MyOwnerLogo.png" alt="">
+            <div class="message">
+                <div class="title">
+                    <p style="font-weight:bold">hubert hirwa</p>
+                    <p style="color:grey;font-weight:bold">just now</p>
+                </div>
+                <p style="color:grey">message will display here after comment!.</p>
+            </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
